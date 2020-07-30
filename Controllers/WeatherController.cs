628185e63185d15d5734c0a63932f39f2032ea47
Fabian Zsofia <fabian.zsofia.eszter@gmail.com>
2020-07-31 00:06:06 +0200

    First controller

---
+++ Controllers/WeatherController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BringYourUmbrella.Controllers
+{
+    public class WeatherController : ControllerBase
+    {
+        [HttpGet]
+        [Route("weather")]
+        public IActionResult Get()
+        {
+            return Ok("nyuszó");
+        }
+    }
+}
